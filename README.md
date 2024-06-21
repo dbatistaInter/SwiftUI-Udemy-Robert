c31a985d33ffff8b4b10dfcfb5327dcc216902bb
dbatistaInter <dbatista@intercam.com.mx>
2024-06-21 13:32:15 -0600

    AsyncImage terminado

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SwiftUI-Udemy-Robert
 Proyectos Generados en el Curso de Udemy con Robert Petras.
+
+- [AsyncImage]()
+    - Cargar y presentar imagen desde URL o placeholder.
+    - Conceptos: AsyncImage, scale, closure de image y placeholder, extension Image, ProgressView animation, transition.
+    - [Preview App]()
--- SwiftUIAsyncImage/SwiftUIAsyncImage/ContentView.swift
+++ SwiftUIAsyncImage/SwiftUIAsyncImage/ContentView.swift
@@ -31,12 +31,42 @@ struct ContentView: View {
             //MARK: - 2. SCALE
             //AsyncImage(url: URL(string: imageURL), scale: 3.0)
             //MARK: - 3. PLACEHOLDER
-            AsyncImage(url: URL(string: imageURL)) { image in
-                image
-                    .imageModifier()
-            } placeholder: {
-                Image(systemName: "photo.circle.fill")
-                    .iconModifier()
+            //            AsyncImage(url: URL(string: imageURL)) { image in
+            //                image
+            //                    .imageModifier()
+            //            } placeholder: {
+            //                Image(systemName: "photo.circle.fill")
+            //                    .iconModifier()
+            //            }
+            //            .padding(40)
+            //
+            //MARK: -4. PHASES
+            //            AsyncImage(url: URL(string: imageURL)) { phase in
+            //                if let image = phase.image {
+            //                    image.imageModifier()
+            //                } else if phase.error != nil {
+            //                    Image(systemName: "ant.circle.fill")
+            //                        .iconModifier()
+            //                } else {
+            //                    Image(systemName: "photo.circle.fill")
+            //                        .iconModifier()
+            //                }
+            //            }
+            //            .padding(40)
+            
+            //MARK: -5. ANIMATION
+            AsyncImage(url: URL(string: imageURL),transaction: Transaction(animation: .spring(response: 0.5,dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+                switch phase {
+                case .success(let image):
+                    image.imageModifier()
+                        .transition(.scale)
+                case .failure(_):
+                    Image(systemName: "ant.circle.fill").iconModifier()
+                case .empty:
+                    Image(systemName: "photo.circle.fill").iconModifier()
+                @unknown default:
+                    ProgressView()
+                }
             }
             .padding(40)
             
